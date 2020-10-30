b065c6326c0f5935932e435759673ed3a99ee757
Zeeshan Bilal <bilalzee@gmail.com>
2020-10-30 00:13:41 +0000

    test

--- files/deploy_app.sh
+++ files/deploy_app.sh
@@ -11,7 +11,7 @@ cat << EOM > /var/www/html/index.html
   <!-- BEGIN -->
   <center><img src="http://${PLACEHOLDER}/${WIDTH}/${HEIGHT}"></img></center>
   <center><h2>Meow World!</h2></center>
-  Welcome to ${PREFIX}'s app. Changes from Github.
+  Welcome to ${PREFIX}'s app. 1 Change from Github.
   <!-- END -->
 
   </div>
